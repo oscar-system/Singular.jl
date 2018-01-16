70e4071a48a4aaeedfd5d156b1b55b6d88ee182d
William Hart <goodwillhart@googlemail.com>
2018-01-16 16:28:06 +0100

    First part of test suite infrastructure.

---
+++ test/number/n_Z-test.jl
@@ -0,0 +1,12 @@
+function test_n_Z_constructors()
+   print("n_Z.constructors...")
+
+   println("PASS")
+end
+
+function test_n_Z()
+   test_n_Z_constructors()
+
+   println("")
+end
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,13 @@
 using Singular
-using Base.Test
 
-# write your own tests here
-@test 1 == 2
+if VERSION < v"0.7.0-DEV.2004"
+   using Base.Test
+else
+   using Test
+end
+
+include("../test/number-test.jl")
+
+test_number()
+
+
