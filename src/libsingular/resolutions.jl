c7592f74e23b61afeada6caa200f169a6a5a5d85
Andreas Steenpass <steenpass@mathematik.uni-kl.de>
2018-01-17 16:05:31 +0100

    add betti()

--- src/libsingular/resolutions.jl
+++ src/libsingular/resolutions.jl
@@ -6,3 +6,27 @@ function getindex(r::resolvente, i::Cint)
    icxx"""(ideal) $r[$i];"""
 end
 
+function syBetti(res::resolvente, length::Cint, R::ring)
+   iv = icxx"""const ring origin = currRing;
+         rChangeCurrRing($R);
+         int dummy;
+         intvec *iv = syBetti($res, $length, &dummy, NULL, FALSE, NULL);
+         rChangeCurrRing(origin);
+         return iv;
+      """
+   nrows = icxx"""$iv->rows();"""
+   ncols = icxx"""$iv->cols();"""
+   betti = icxx"""int **betti = (int **)malloc($ncols*sizeof(int *));
+         betti[0] = (int *)malloc($ncols*$nrows*sizeof(int));
+         for (int i = 0; i < $ncols; i++) {
+            betti[i] = (*betti + $nrows*i);
+         }
+         for (int i = 0; i < $ncols; i++) {
+            for (int j = 0; j < $nrows; j++) {
+               betti[i][j] = IMATELEM(*$iv, j+1, i+1);
+            }
+         }
+         return &betti[0][0];
+      """
+   unsafe_wrap(Array, betti, (nrows, ncols), true)
+end
