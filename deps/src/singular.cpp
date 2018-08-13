20761091396c16a2607f2489cfaa68e3819308a7
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2018-08-13 14:31:04 +0200

    Added first CMake Singular CxxWrap build

---
+++ deps/src/singular.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <iostream>
+
+#include "jlcxx/jlcxx.hpp"
+
+#include <Singular/libsingular.h>
+#include <Singular/cntrlc.h>
+
+
+void test_init(const char* test){
+    siInit(const_cast<char*>(test));
+}
+
+const char* test_versionString(){
+    return versionString();
+}
+
+JULIA_CPP_MODULE_BEGIN(registry)
+  jlcxx::Module& Singular = registry.create_module("Singular");
+  Singular.method("si_init",&test_init);
+  Singular.method("versionString",&test_versionString);
+
+JULIA_CPP_MODULE_END
