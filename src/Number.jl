0cb98c0f9a05cfab43e2cca16e3b09ff6cf1c93f
William Hart <goodwillhart@googlemail.com>
2018-08-14 14:15:18 +0200

    Get rid of n_Z.cxx.

--- src/Number.jl
+++ src/Number.jl
@@ -2,7 +2,7 @@ include("number/NumberTypes.jl")
 
 # include("number/n_Q.jl")
 
-include("number/n_Z.jl")
+# include("number/n_Z.jl")
 
 # include("number/n_Zn.jl")
 
--- src/number/NumberTypes.jl
+++ src/number/NumberTypes.jl
@@ -33,3 +33,114 @@ function _Integers_clear_fn(cf::Integers)
    end
 end
 
+type n_Z <: Nemo.RingElem
+    ptr::libSingular.number
+
+    function n_Z()
+        const c = ZZ.ptr
+        z = new(libSingular.n_Init(0, c))
+        parent(z).refcount += 1
+        finalizer(z, _n_Z_clear_fn)
+        return z
+    end
+
+    function n_Z(n::Int)
+        const c = ZZ.ptr
+        z = new(libSingular.n_Init(n, c))
+        parent(z).refcount += 1
+        finalizer(z, _n_Z_clear_fn)
+        return z
+    end
+
+    function n_Z(n::libSingular.number)
+        z = new(n)
+        parent(z).refcount += 1
+        finalizer(z, _n_Z_clear_fn)
+        return z
+    end
+end
+
+function _n_Z_clear_fn(n::n_Z)
+   R = parent(n)
+   libSingular.n_Delete(n.ptr, parent(n).ptr)
+   _Integers_clear_fn(R)
+   nothing
+end
+
+###############################################################################
+#
+#   Rationals/n_Q
+#
+###############################################################################
+
+function get_n_Q()
+   d = libSingular.nInitChar(libSingular.n_Q, Ptr{Void}(0))
+end
+
+const RationalsID = Dict{Symbol, Field}()
+
+type Rationals <: Field
+   ptr::libSingular.coeffs
+   refcount::Int
+
+   function Rationals()
+      if haskey(RationalsID, :QQ)
+         d = RationalsID[:QQ]::Rationals
+      else
+         d = new()
+         RationalsID[:QQ] = d
+         finalizer(d, _Rationals_clear_fn)
+      end
+      return d
+   end
+end
+
+function _Rationals_clear_fn(cf::Rationals)
+   cf.refcount -= 1
+   if cf.refcount == 0
+      libSingular.nKillChar(cf.ptr)
+   end
+   nothing
+end
+
+type n_Q <: Nemo.FieldElem
+    ptr::libSingular.number
+
+    function n_Q()
+        const c = QQ.ptr
+        z = new(libSingular.n_Init(0, c))
+        parent(z).refcount += 1
+        finalizer(z, _n_Q_clear_fn)
+        return z
+    end
+
+    function n_Q(n::Int)
+        const c = QQ.ptr
+        z = new(libSingular.n_Init(n, c))
+        parent(z).refcount += 1
+        finalizer(z, _n_Q_clear_fn)
+        return z
+    end
+
+    function n_Q(n::n_Z)
+        z = new(libSingular.nApplyMapFunc(n_Z_2_n_Q, n.ptr, ZZ.ptr, QQ.ptr))
+        parent(z).refcount += 1
+        finalizer(z, _n_Q_clear_fn)
+        return z
+    end
+
+    function n_Q(n::libSingular.number)
+        z = new(n)
+        parent(z).refcount += 1
+        finalizer(z, _n_Q_clear_fn)
+        return z
+    end
+end
+
+function _n_Q_clear_fn(n::n_Q)
+   R = parent(n)
+   libSingular.n_Delete(n.ptr, parent(n).ptr)
+   _Rationals_clear_fn(R)
+   nothing
+end
+
