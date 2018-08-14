83a088742ebd7cfd7f249e6c905138e253687f4a
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2018-08-14 17:50:23 +0200

    Fixed include dir for nemo

--- deps/build.jl
+++ deps/build.jl
@@ -116,7 +116,7 @@ cmake_build_path = Pkg.dir("Singular","deps","src")
 
 cd(cmake_build_path)
 
-run(`cmake -DJulia_EXECUTABLE=$julia_exec -DJlCxx_DIR=$jlcxx_cmake_dir -DJuliaIncludeDir=$julia_include -DJULIA_LIB_DIR=$julia_lib -Dsingular_includes=$vdir/include -Dsingular_libdir=$vdir/lib -DCMAKE_INSTALL_LIBDIR=$vdir/lib .`)
+run(`cmake -DJulia_EXECUTABLE=$julia_exec -DJlCxx_DIR=$jlcxx_cmake_dir -DJuliaIncludeDir=$julia_include -DJULIA_LIB_DIR=$julia_lib -Dnemo_includes=$nemovdir/include -Dsingular_includes=$vdir/include -Dsingular_libdir=$vdir/lib -DCMAKE_INSTALL_LIBDIR=$vdir/lib .`)
 
 run(`make`)
 run(`make install`)
