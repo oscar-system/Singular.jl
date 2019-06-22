28b4890e9d096f66dd10c7bda46d1579e51d6924
Sebastian Gutsche <gutsche@mathematik.uni-siegen.de>
2019-06-22 21:37:44 +0200

    Added interface to Singular interpreter
    via strings in Singular language

---
+++ test/call_interpreter-test.jl
@@ -0,0 +1,14 @@
+function test_call_interpreter()
+    print("call_interpreter...")
+    Singular.libSingular.call_interpreter("ring r;")
+    result = Singular.libSingular.call_interpreter("r;")
+    @test result[1] == false
+    Singular.libSingular.call_interpreter("poly f = x;")
+    result = Singular.libSingular.call_interpreter("f;")
+    @test result[1] == false
+    @test result[2] == "x\n"
+    @test result[3] == ""
+    @test result[4] == ""
+    @test length(result) == 4
+    println("PASS")
+end
