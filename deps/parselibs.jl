bb689564c888d5ed0b50e97cd1f941cb05c80aa0
Max Horn <max@quendi.de>
2020-04-14 00:46:05 +0200

    Properly restore working directory in parselibs.jl

--- deps/parselibs.jl
+++ deps/parselibs.jl
@@ -13,8 +13,6 @@ end
 
 libparsepath = abspath(joinpath(@__DIR__, "usr", "bin", "libparse"))
 
-library_dir = ""
-
 if haskey(ENV, "SINGULAR_LIBRARY_DIR")
     library_dir = ENV["SINGULAR_LIBRARY_DIR"]
 else
@@ -33,26 +31,23 @@ output_filename = abspath(joinpath(@__DIR__, "..", "src", "libraryfuncdictionary
   All other columns (containing info such as line numbers, library name, etc)
   are ignored.
 =#
-olddir = pwd()
-cd(abspath(joinpath(@__DIR__, "usr", "bin")))
-
-open(output_filename, "w") do outputfile
-    println(outputfile, "libraryfunctiondictionary = Dict(")
-    for i in filenames
-        full_path = joinpath(library_dir, i)
-        libs = execute(`$libparsepath $full_path`)
-        if libs.stderr != ""
-            error("from libparse: $(libs.stderr)")
+cd(abspath(joinpath(@__DIR__, "usr", "bin"))) do
+    open(output_filename, "w") do outputfile
+        println(outputfile, "libraryfunctiondictionary = Dict(")
+        for i in filenames
+            full_path = joinpath(library_dir, i)
+            libs = execute(`$libparsepath $full_path`)
+            if libs.stderr != ""
+                error("from libparse: $(libs.stderr)")
+            end
+            libs_splitted = split(libs.stdout,"\n")[4:end - 1]
+            libs_splitted = [split(i, " ", keepempty = false) for i in libs_splitted]
+            println(outputfile, ":$(i[1:end - 4]) => [")
+            for j in libs_splitted
+                println(outputfile, """[ "$(j[1])", "$(j[3])" ],""")
+            end
+            println(outputfile, "],\n")
         end
-        libs_splitted = split(libs.stdout,"\n")[4:end - 1]
-        libs_splitted = [split(i, " ", keepempty = false) for i in libs_splitted]
-        println(outputfile, ":$(i[1:end - 4]) => [")
-        for j in libs_splitted
-            println(outputfile, """[ "$(j[1])", "$(j[3])" ],""")
-        end
-        println(outputfile, "],\n")
+        println(outputfile, ")\n")
     end
-    println(outputfile, ")\n")
 end
-
-cd(pwd)
