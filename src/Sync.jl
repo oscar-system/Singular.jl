c0c6ea483d7d8e93302e7320f567eea791a5e3d4
Reimer Behrends <behrends@gmail.com>
2020-08-14 03:17:29 +0200

    Fix typevar processing.

--- src/Sync.jl
+++ src/Sync.jl
@@ -1,5 +1,17 @@
 module Sync
 
+    using Singular
+    using AbstractAlgebra
+    using Nemo
+    using LinearAlgebra
+    using CxxWrap
+    using SuiteSparse
+    using SharedArrays
+    using SparseArrays
+
+    # We need to import reduce() because we will later overload it.
+    import Base: reduce
+
     const _mutex = ReentrantLock()
 
     function _lock()
@@ -11,37 +23,151 @@ module Sync
     end
 
     function _wrap_methods(mod::Module, fn::Function)
+        function qualified_typename(typename)
+            if typename isa Core.TypeName
+              reduce((a, b) -> :($a.$b),
+                  [ fullname(typename.module)..., typename.name ])
+            else
+              typename
+            end
+        end
+        function bounded(typevar::TypeVar)
+            ub = strip_type_bounds(typevar.ub)
+            name = typevar.name
+            lb = typevar.lb
+            if typevar.lb === Union{}
+                if ub === Any
+                    :( $(name) )
+                else
+                    :( $(name) <: $(ub) )
+                end
+            else
+                lb = strip_type_bounds(lb)
+                :( $(lb) <: $(name) <: $(ub) )
+            end
+        end
+        function isunbounded(typevar::TypeVar)
+            typevar.lb === Union{} && typevar.ub === Any
+        end
+        function union_subtypes_aux(type)
+            if type.b isa Union
+              [ type.a, union_subtypes(type.b)... ]
+            else
+              [ type.a, type.b ]
+            end
+        end
+        function union_subtypes(type)
+            map(strip_type_bounds, union_subtypes_aux(type))
+        end
+        # The function signature is annotated with superfluous type
+        # bounds that we need to strip out in order for it to compile
+        # again.
+        # Example:
+        #
+        # f(x::X{T}) where {T <: Int}
+        #
+        # becomes
+        #
+        # f(x::X{T <: Int}) where {T <: Int}
+        #
+        # internally. Similarly, if Y has two type parameters, then
+        #
+        # Y{T} becomes Y{T, S} where S internally.
+        function strip_type_bounds(type)
+            if type isa TypeVar
+                # basic case
+                type.name
+            elseif type isa UnionAll
+                if Base.print_without_params(type)
+                    # early return for types like RoundMode or Rational.
+                    return type
+                end
+                # type is basic parameterized type represented in a
+                # UnionAll linked list, with type.var denoting the
+                # type parameter and type.body the tail of the list.
+                # First, we unwrap it analogously to Base.unwrap_unionall(),
+                # but preserving type parameters.
+                vars = [ type.var ]
+                type = type.body
+                while type isa UnionAll
+                    push!(vars, type.var)
+                    type = type.body
+                end
+                if type === Union{}
+                    # bottom type
+                    type
+                elseif type isa Union
+                    # union type
+                    :(Union{$(union_subtypes(type)...)}
+                        where {$(map(bounded, vars)...)})
+                else
+                    # X{...} where X is not a union type.
+                    parameters = [ type.parameters... ]
+                    # strip off trailing parameters in types of the
+                    # kind Y{T, S} where S.
+                    while !isempty(parameters) && !isempty(vars) &&
+                            last(parameters) == last(vars) &&
+                            isunbounded(last(vars))
+                        pop!(vars)
+                        pop!(parameters)
+                    end
+                    # type.name is not necessarily a symbol, but a
+                    # qualified type, e.g. Module.T, and we need to
+                    # reconstruct it as an Expr().
+                    typename = qualified_typename(type.name)
+                    # Return type.
+                    :($(typename){$(map(strip_type_bounds, parameters)...)}
+                        where {$(map(bounded, vars)...)})
+                end
+            elseif type isa Union
+                    # unparameterized union type
+                    :(Union{$(union_subtypes(type)...)})
+            elseif type isa DataType
+                if length(type.parameters) == 0
+                    type
+                else
+                    # parameterized type, but all parameters are concrete.
+                    # we do not need to manage typevars of the type, but
+                    # the overall function may still have typevars, so
+                    # we still need to strip bounds recursively.
+                    if type isa Union
+                        :(Union{$(union_subtypes(type)...)})
+                    else
+                        typename = qualified_typename(type.name)
+                        :($(typename){$(map(strip_type_bounds,
+                            type.parameters)...)})
+                    end
+                end
+            elseif type isa Symbol
+                # This is a special case for Val{:lex} types, as
+                # symbols are treated differently in quoted expressions.
+                Meta.quot(type)
+            else
+                type
+            end
+        end
         for method in methods(fn).ms
             funcname = method.name
             # Functions that have type variables store them in a linked list
             # in their signature, where sig.var is the type variable and
             # sig.body is the link to the next item in the list. Such linked
-            # lists are of type UnionAll.
+            # lists are of type UnionAll. See Base.unwrap_unionall() and
+            # related functions for similar functionality.
             typevars = []
             sig = method.sig
             while sig isa UnionAll
                 typevar = sig.var
-                push!(typevars, :( $(typevar.lb) <: $(typevar.name) <: $(typevar.ub) ))
+                push!(typevars, bounded(typevar))
                 sig = sig.body
             end
             # After stripping out all the type variables, the remaining
             # signature contains the function type and the argument types.
             resulttype = sig.parameters[1]
             argtypes = []
+            rawargtypes = []
             for argtype in sig.parameters[2:end]
-                # We need to strip type bounds from types involving type
-                # variables, as they are already part of the where clause.
-                #
-                # TODO: This does not handle the case T{A<:Bound} correctly,
-                # only T<:Bound.
-                #
-                # Parameterized types with type variables are again
-                # represented as UnionAll linked lists.
-                if argtype isa TypeVar
-                    push!(argtypes, argtype.name)
-                else
-                    push!(argtypes, argtype)
-                end
+                push!(rawargtypes, argtype)
+                push!(argtypes, strip_type_bounds(argtype))
             end
             has_kwargs = length(Base.kwarg_decl(method)) > 0
             is_vararg = method.isva
@@ -62,10 +188,14 @@ module Sync
                 else
                     push!(argnames, argname)
                 end
-                push!(args, :($argname :: $argtype))
+                push!(args, :($argname :: $(argtype)))
             end
-            # We don't enumerate keyword args; they are processed in the
-            # body of the wrapped method, anyway.
+            # TODO: We don't enumerate keyword args; they are processed
+            # in the body of the wrapped method, anyway. However, this
+            # impedes tab completion with using the REPL.
+            #
+            # The main challenge is extracting the name of the arguments,
+            # as the names can contain additional type information.
             kwargs = []
             if has_kwargs
                 push!(kwargs, :(kw...))
@@ -102,6 +232,7 @@ module Sync
         end
 
 
+        # num_errors = 0
         for name in names(mod; all = true)
             def = find_def(name)
             if def isa Function
@@ -109,7 +240,7 @@ module Sync
                     _wrap_methods(mod, def)
                 catch
                     # debugging code
-                    # println("error wrapping $name")
+                    println("error wrapping $name[$method_num]")
                     for (exception, backtrace) in Base.catch_stack()
                         # showerror(stdout, exception, backtrace)
                         # println()
@@ -117,6 +248,7 @@ module Sync
                 end
             end
         end
+        # println("error count: $(num_errors)")
     end
 
 end
@@ -129,7 +261,7 @@ end
         # f(x::T, Y::U) where {T, U} = 0
         display2(;alpha = 1, kw...) = println("kw: alpha = $(alpha); $(kw)")
         display3(x::T) where {T <: Int} = println("varargs: $(x)")
-        display4(x...) = println(x)
+        display4(x...) where Q = println(x)
     end))
 
     Sync._wrap_module(WrapExample)
