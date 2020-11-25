05f0a1886facd4dc899660e2a315b769a98f2fbd
Max Horn <max@quendi.de>
2020-11-25 10:38:10 +0100

    Refactor libraryfuncdictionary.jl regeneration
    ... by moving it into a function, so that it can also be invoked
    manually (useful for debugging) and also it doesn't pollute the
    module namespace.

--- src/setup.jl
+++ src/setup.jl
@@ -68,48 +68,52 @@ function execute(libparse, path)
   end
 end
 
-libparsepath = abspath(joinpath(prefixpath, "bin", "libparse"))
+function regenerate_libraryfuncdictionary(prefixpath)
 
-if haskey(ENV, "SINGULAR_LIBRARY_DIR")
-    library_dir = ENV["SINGULAR_LIBRARY_DIR"]
-else
-    library_dir = abspath(joinpath(prefixpath, "share", "singular", "LIB"))
-end
+    libparsepath = abspath(joinpath(prefixpath, "bin", "libparse"))
 
-filenames = filter(x -> endswith(x, ".lib"), readdir(library_dir))
+    if haskey(ENV, "SINGULAR_LIBRARY_DIR")
+        library_dir = ENV["SINGULAR_LIBRARY_DIR"]
+    else
+        library_dir = abspath(joinpath(prefixpath, "share", "singular", "LIB"))
+    end
 
-output_filename = abspath(joinpath(@__DIR__, "libraryfuncdictionary.jl"))
+    filenames = filter(x -> endswith(x, ".lib"), readdir(library_dir))
 
-@info "Regenerating $(output_filename)"
+    output_filename = abspath(joinpath(@__DIR__, "libraryfuncdictionary.jl"))
 
-#=
-  Loops over all libraries and executes libparse on it.
-  The first three lines of the libparse output are general information
-  about the library, so we ignore it. We are only interested in the
-  first column (library name) and the third column (globally exposed or not).
-  All other columns (containing info such as line numbers, library name, etc)
-  are ignored.
-=#
-cd(abspath(joinpath(prefixpath, "bin"))) do
-    open(output_filename, "w") do outputfile
-        println(outputfile, "libraryfunctiondictionary = Dict(")
-        for i in filenames
-            full_path = joinpath(library_dir, i)
-            libs = execute(Singular_jll.libparse, full_path)
-            if libs.stderr != ""
-                error("from libparse: $(libs.stderr)")
+    @info "Regenerating $(output_filename)"
+
+    #=
+      Loops over all libraries and executes libparse on it.
+      The first three lines of the libparse output are general information
+      about the library, so we ignore it. We are only interested in the
+      first column (library name) and the third column (globally exposed or not).
+      All other columns (containing info such as line numbers, library name, etc)
+      are ignored.
+    =#
+    cd(abspath(joinpath(prefixpath, "bin"))) do
+        open(output_filename, "w") do outputfile
+            println(outputfile, "libraryfunctiondictionary = Dict(")
+            for i in filenames
+                full_path = joinpath(library_dir, i)
+                libs = execute(Singular_jll.libparse, full_path)
+                if libs.stderr != ""
+                    error("from libparse: $(libs.stderr)")
+                end
+                libs_splitted = split(libs.stdout,"\n")[4:end - 1]
+                libs_splitted = [split(i, " ", keepempty = false) for i in libs_splitted]
+                println(outputfile, ":$(i[1:end - 4]) => [")
+                for j in libs_splitted
+                    println(outputfile, """[ "$(j[1])", "$(j[3])" ],""")
+                end
+                println(outputfile, "],\n")
             end
-            libs_splitted = split(libs.stdout,"\n")[4:end - 1]
-            libs_splitted = [split(i, " ", keepempty = false) for i in libs_splitted]
-            println(outputfile, ":$(i[1:end - 4]) => [")
-            for j in libs_splitted
-                println(outputfile, """[ "$(j[1])", "$(j[3])" ],""")
-            end
-            println(outputfile, "],\n")
+            println(outputfile, ")\n")
         end
-        println(outputfile, ")\n")
     end
 end
 
+regenerate_libraryfuncdictionary(Singular_jll.find_artifact_dir())
 
 end # module
