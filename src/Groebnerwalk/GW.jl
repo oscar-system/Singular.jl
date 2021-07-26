c156d921e3e1538d2c26592f2abc67a83be4a23f
jwelp <welpj@gmx.de>
2021-07-26 09:52:17 +0200

    setup

---
+++ src/Groebnerwalk/GW.jl
@@ -0,0 +1,177 @@
+using Oscar
+
+
+
+###############################################################
+#Implementation of the gröbner walk.
+#TODO: Implement pertubed version.
+#TODO: Implement generic version.
+#TODO: Implement fractal version.
+###############################################################
+
+
+###############################################################
+#Top-Level
+#TODO: Implement input checks
+#Fragen:
+#Singular.reduce für f % G selbst programmieren?
+#Interreduction selbst endgültig programmieren programmieren?
+#Für divrem in Oscar-Ideal: sinnvoller möglich?
+#Arithmetik von Polynomen -> Immer MPolyBuildCTX?
+###############################################################
+
+function groebner_walk(
+    G::Singular.sideal,
+    S::Matrix{Int64},
+    T::Matrix{Int64},
+    grwalktype::Symbol = :standard,
+)
+
+    if grwalktype == :standard
+        walk = (x, y, z) -> standard_walk(x, y, z)
+    elseif grwalktype == :generic
+        walk = (x, y, z) -> generic_walk(x, y, z)
+    elseif grwalktype == :pertubed
+        walk = (x, y, z) -> pertubed_walk(x, y, z)
+    elseif grwalktype == :fractal
+        walk = (x, y, z) -> fractal_walk(x, y, z)
+        #error("Choose a strategy from: :generic, :standard ...")
+        # fill the list or implement a functionality to choose the best
+        # way w.r.t. the type of the ideal
+    end
+
+    ######TODO:Check the parameter#####
+    R = singular_ring(base_ring(G))
+    I = Singular.Ideal(R, [R(x) for x in gens(G)])
+
+    return walk(I, S, T)
+end
+
+
+###############################################################
+#Standard-version of the groebner walk by Collart et al
+###############################################################
+function standard_walk(G::Singular.sideal, S::Matrix{Int64}, T::Matrix{Int64})
+    cweight = S[1, :]
+    tweight = T[1, :]
+    G = standard_step(I, cweight, tweight, T)
+
+    #loop
+    while cweight != tweight
+        cweight = nextw(G, cweight, tweight)
+        G = standard_step(G, cweight, tweight, T)
+        #@info "Current Gröbnerbase: " G
+    end
+
+    #finalization
+    S, V = change_order(G, T)
+    return Oscar.Singular.Ideal(S, [change_ring(gen, S) for gen in gens(G)])
+
+end
+
+function standard_step(
+    G::Singular.sideal,
+    cw::Array{Int64,1},
+    tw::Array{Int64,1},
+    T::Matrix{Int64},
+)
+    R = base_ring(G)
+    S, V = change_order(G, cw, T)
+    inwG = initials(S, gens(G), cw)
+    IinwG = Singular.std(
+        Oscar.Singular.Ideal(S, [S(x) for x in inwG]),
+        complete_reduction = true,
+    )
+
+    #Lifting to GB of new cone
+    rest = [
+        gen - change_ring(Oscar.Singular.reduce(change_ring(gen, R), G), S)
+        for gen in gens(IinwG)
+    ]
+    Gnew = Oscar.Singular.Ideal(S, [S(x) for x in rest])
+    Gnew.isGB = true
+
+    return Oscar.Singular.std(Gnew, complete_reduction = true)
+end
+
+###############################################################
+#Generic-version of the groebner walk by Fukuda et al. (2007)
+###############################################################
+
+
+function generic_walk(G::Singular.sideal, S::Matrix{Int64}, T::Matrix{Int64})
+    R, V = change_order(G, T)
+
+    v = nextV(G, [0], S, T)
+    lm = [Singular.leading_monomial(g) for g in gens(G)]
+
+    while !isempty(v)
+        G, lm = generic_step(G, R, v, T, lm)
+        @info "Current Gröbnerbase: " G
+        v = nextV(G,lm, v, S, T)
+    end
+    G = Oscar.Singular.Ideal(R, [change_ring(x, R) for x in gens(G)])
+    return G
+end
+
+function generic_step(
+    G::Singular.sideal,
+    S::MPolyRing,
+    v::Vector{Int64},
+    T::Matrix{Int64},
+    lm::Vector{Singular.spoly{Singular.n_unknown{fmpq}}},
+)
+    R = base_ring(G)
+    facet_Generators = facet_initials(S, G, v, lm)
+
+    facet_Ideal = Singular.std(
+        Oscar.Singular.Ideal(S, [S(x) for x in facet_Generators]),
+        complete_reduction = true,
+    )
+
+    println("Facet Ideal: ", facet_Ideal)
+
+    liftArray, Newlm = liftgeneric(G, lm, facet_Ideal)
+    Gnew = Oscar.Singular.Ideal(S, [S(x) for x in liftArray])
+
+    #println("New lifted GB:   ", Gnew, "  with LM:  ", Newlm)
+    Gnew = interreduce_new(Gnew, Newlm)
+    Gnew = Oscar.Singular.Ideal(R, [change_ring(x, R) for x in gens(Gnew)])
+    Gnew.isGB = true
+    return Gnew, Newlm
+end
+
+
+###############################################################
+#Fractal-version of the groebner walk by Amrhein & Gloor (200)
+###############################################################
+
+function fractal_walk(
+    G::Singular.sideal,
+    cweight::Array{Int,1},
+    tweight::Array{Int,1},
+    tord::Symbol,
+)
+
+    T = ordering_as_Matrix(tweight, tord)
+    M  = nextw(G, cweight, tweight)
+    R = base_ring(G)
+    S, V = change_order(G, cw, tw, ordering)
+
+    inwG = initials(S, gens(G), cw)
+
+    IinwG = Singular.std(
+        Oscar.Singular.Ideal(S, [S(x) for x in inwG]),
+        complete_reduction = true,
+    )
+    G.isGB = true
+
+    #Lifting to GB of new cone
+    rest = [
+        gen - change_ring(Oscar.Singular.reduce(change_ring(gen, R), G), S)
+        for gen in gens(IinwG)
+    ]
+    Gnew = Oscar.Singular.Ideal(S, [S(x) for x in rest])
+    Gnew.isGB = true
+    return std(Gnew, complete_reduction = true)
+end
---
+++ src/Groebnerwalk/GWtest.jl
@@ -0,0 +1,149 @@
+using Pkg
+
+using Oscar
+
+
+case = 4
+
+
+if case == 1 || case == 99
+    R, (x, y) = Oscar.Singular.PolynomialRing(
+        QQ,
+        ["x", "y"],
+        ordering = Singular.ordering_M(ordering_as_matrix(:degrevlex, 2)),
+    )
+
+    f1 = x^2 - y^3
+    f2 = x^3 - y^2 - x
+    I = Singular.Ideal(R, [f1, f2])
+
+    I = Oscar.Singular.std(I, complete_reduction = true)
+
+    S, V = Oscar.Singular.PolynomialRing(QQ, ["x", "y"], ordering = :lex)
+
+    J = groebner_walk(
+        I,
+        ordering_as_matrix(:degrevlex, 2),
+        ordering_as_matrix(:lex, 2),
+        :generic,
+    )
+    T1 = Oscar.Singular.Ideal(S, [change_ring(x, S) for x in gens(J)])
+
+    S, V = Oscar.Singular.PolynomialRing(QQ, ["x", "y"], ordering = :lex)
+    T2 = Singular.std(
+        Oscar.Singular.Ideal(S, [change_ring(x, S) for x in gens(I)]),
+        complete_reduction = true,
+    )
+
+    println("test 1: ", equalitytest(T1, T2))
+end
+
+if case == 2 || case == 99
+
+    R, (x, y, z) = Oscar.Singular.PolynomialRing(
+        QQ,
+        ["x", "y", "z"],
+        ordering = Singular.ordering_M(ordering_as_matrix([5, 4, 1], :deglex)),
+    )
+
+    f1 = x^2 - y
+    f2 = y^2 - x * z - y * z
+    I = Singular.Ideal(R, [f1, f2])
+
+    I = Oscar.Singular.std(I, complete_reduction = true)
+
+
+    J = groebner_walk(
+        I,
+        ordering_as_matrix([5, 4, 1], :deglex),
+        ordering_as_matrix([6, 1, 3], :lex),
+        :generic,
+    )
+
+    S, V = Oscar.Singular.PolynomialRing(
+        QQ,
+        ["x", "y", "z"],
+        ordering = Singular.ordering_M(ordering_as_matrix([6, 1, 3], :lex)),
+    )
+    T1 = Singular.std(
+        Oscar.Singular.Ideal(S, [change_ring(x, S) for x in gens(J)]),
+        complete_reduction = true,
+    )
+    T2 = Singular.std(
+        Oscar.Singular.Ideal(S, [change_ring(x, S) for x in gens(I)]),
+        complete_reduction = true,
+    )
+
+    println("test 2: ", equalitytest(T1, T2))
+
+end
+
+
+if case == 3 || case == 99
+    R, (x, y) = Oscar.Singular.PolynomialRing(
+        QQ,
+        ["x", "y"],
+        ordering = Singular.ordering_M(ordering_as_matrix(:deglex, 2)),
+    )
+
+    f1 = x^2 - y^3
+    f2 = x^3 - y^2 - x
+    I = Singular.Ideal(R, [f1, f2])
+
+    I = Oscar.Singular.std(I, complete_reduction = true)
+
+    T1 = groebner_walk(
+        I,
+        ordering_as_matrix(:deglex, 2),
+        ordering_as_matrix(:lex, 2),
+        :generic,
+    )
+    S, V = Oscar.Singular.PolynomialRing(
+        QQ,
+        ["x", "y"],
+        ordering = Oscar.Singular.ordering_M([1 0; 0 1]),
+    )
+    T2 = Singular.std(
+        Oscar.Singular.Ideal(S, [change_ring(x, S) for x in gens(I)]),
+        complete_reduction = true,
+    )
+    println("test 3: ", equalitytest(T1, T2))
+
+end
+
+if case == 4 || case == 99
+    R, (x, y, z, u, v, w) = Oscar.Singular.PolynomialRing(
+        QQ,
+        ["x", "y", "z", "u", "v", "w"],
+        ordering = Singular.ordering_M(ordering_as_matrix(:degrevlex, 6)),
+    )
+
+
+    f1 = x * y^3 + y^4 + y * z^2 - u^2 #* y * x * w^3
+    f2 = 2 * x^2 * y + x^3 * y + 2 #* w * y^2 * z #+ u * x * v^4
+    f3 = 2 - 3 * x^2 * v * w #+ #u+ w^2 #* y + v^3
+    I = Singular.Ideal(R, [f1, f2, f3])
+    I = Oscar.Singular.std(I, complete_reduction = true)
+
+
+    @time J = groebner_walk(
+        I,
+        ordering_as_matrix(:degrevlex, 6),
+        ordering_as_matrix(:lex, 6),
+        :generic
+    )
+
+
+    S, V = Oscar.Singular.PolynomialRing(
+        QQ,
+        ["x", "y", "z", "u", "v", "w"],
+        ordering = Oscar.Singular.ordering_M(ordering_as_matrix(:lex, 6)),
+    )
+    @time T1 = Singular.std(
+        Oscar.Singular.Ideal(S, [change_ring(x, S) for x in gens(I)]),
+        complete_reduction = true,
+    )
+    T2 = Oscar.Singular.Ideal(S, [change_ring(x, S) for x in gens(J)])
+
+    println("test 4: ", equalitytest(T1, T2))
+end
