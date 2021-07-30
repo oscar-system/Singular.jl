cf56be780c244d9084db81a76af94c65e944c323
Daniel Schultz <tthsqe12@gmail.com>
2021-07-30 17:52:53 +0200

    start singular's bigint and bigintmat

---
+++ src/matrix/bigintmat.jl
@@ -0,0 +1,53 @@
+# The Singular bigintmat is in an unfortunate place. The entries belong
+# to coeffs_BIGINT and not to libSingular's n_Z. Therefore, it does not make
+# much sense to have a constructor
+#   matrix(n_Z, [1 2; 3 4])
+# return a matrix over the wrong integers. Nor does it make sense to make a new
+# ring of integers for use in Singular.jl. Therefore, we keep the api very
+# simple and allow conversions between fmpz_mat and Matrix{BigInt}.
+
+function sbigintmat(r::Int, c::Int)
+    return sbigintmat(libSingular.bigintmat_init(r, c))
+end
+
+function ncols(m::sbigintmat)
+    return Int(libSingular.bigintmat_ncols(m.ptr))
+end
+
+function nrows(m::sbigintmat)
+    return Int(libSingular.bigintmat_nrows(m.ptr))
+end
+
+function getindex(m::sbigintmat, i::Int, j::Int)
+   (0 < i <= nrows(m) && 0 < j <= ncols(m)) || error("index out of range")
+   GC.@preserve m begin
+      # not our own ptr
+      ptr = libSingular.bigintmat_viewindex(m.ptr, Cint(i), Cint(j))
+      return libSingular.n_GetMPZ(ptr, libSingular.get_coeffs_BIGINT())
+   end
+end
+
+function setindex!(m::sbigintmat, a::Union{BigInt, fmpz}, i::Int, j::Int)
+   (0 < i <= nrows(m) && 0 < j <= ncols(m)) || error("index out of range")
+   # rawset consumes our ptr
+   ptr = libSingular.number_ptr(a, libSingular.get_coeffs_BIGINT())
+   GC.@preserve m libSingular.bigintmat_rawset(m.ptr, ptr, Cint(i), Cint(j))
+end
+
+function show(io::IO, M::sbigintmat)
+   print(io, "[")
+   m = nrows(M)
+   n = ncols(M)
+   for i = 1:m
+      for j = 1:n
+         show(io, M[i, j])
+         if j != n
+            print(io, ", ")
+         elseif i != m
+            println(io, "")
+         end
+      end
+   end
+   print(io, "]")
+end
+
