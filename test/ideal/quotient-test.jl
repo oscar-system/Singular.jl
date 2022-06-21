40d6cabfd9c0c9772f521634c84d1e32017ab4d4
dan <tthsqe12@gmail.com>
2022-06-21 10:44:21 +0200

    test no GB on quotient

--- test/ideal/quotient-test.jl
+++ test/ideal/quotient-test.jl
@@ -1,3 +1,13 @@
+@testset "quotient.PolynomialRing" begin
+   R, (x, y, z) = @inferred PolynomialRing(QQ, ["x", "y", "z"])
+   Q, (x, y, z) = @inferred QuotientRing(R, Ideal(R, x^2 - y*z))
+
+   @test is_quotient_ring(Q)
+
+   I = Ideal(Q, x)
+   @test !I.isGB
+end
+
 @testset "quotient.GAlgebra" begin
 
    R, _ = @inferred PolynomialRing(QQ, ["z", "u", "v", "w"])
