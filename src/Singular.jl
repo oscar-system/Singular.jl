af289763b81d1b8279b096403c7e880a3221adda
Max Horn <max@quendi.de>
2023-06-14 10:02:09 +0200

    Improve banner hiding code (#668)
    ... based on what we do in GAP.jl

--- src/Singular.jl
+++ src/Singular.jl
@@ -166,10 +166,25 @@ function __init__()
    merge!(mapping_types_reversed, Dict( i[2] => i[1] for i in libSingular.get_type_mapper() ))
    merge!(casting_functions, create_casting_functions())
 
+   # Check if were loaded from another package
+   # if VERSION < 1.7.*, only the "other" package will have the
+   # _tryrequire_from_serialized in the backtrace.
+   # if VERSION >= 1.8, also doing 'using Package' will have
+   # _tryrequire_from_serialized the backtrace.
+   #
+   # To still distinguish both scenarios, notice that
+   # 'using OtherPackage' will either have _tryrequire_from_serialized at least twice,
+   # or one with four arguments (hence five as the function name is the first argument)
+   # 'using Package' serialized will have a version with less arguments
+   bt = Base.process_backtrace(Base.backtrace())
+   filter!(sf -> sf[1].func === :_tryrequire_from_serialized, bt)
+   isinteractive_manual =
+     length(bt) == 0 || (length(bt) == 1 && length(only(bt)[1].linfo.specTypes.parameters) < 4)
+
    # Respect the -q and --banner flag
    allowbanner = Base.JLOptions().banner != 0
 
-   show_banner = allowbanner && isinteractive() &&
+   show_banner = allowbanner && isinteractive_manual && isinteractive() &&
                 !any(x->x.name in ["Oscar"], keys(Base.package_locks)) &&
                 get(ENV, "SINGULAR_PRINT_BANNER", "true") != "false"
 
