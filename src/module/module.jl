e92cc53958744ffd0a6a9671a900440bb4b24c4b
ederc <ederc@mathematik.uni-kl.de>
2023-08-07 11:08:07 +0200

    fixes typo

--- src/module/module.jl
+++ src/module/module.jl
@@ -204,7 +204,7 @@ end
 
 Computes a division with remainder of the generators of `I` by
 the generators of `G`. Returns a tuple (Quo, Rem, U) where
-`Matrix(I)*MAtrix(U) = Matrix(G)*Matrix(Quo) + Matrix(Rem)`
+`Matrix(I)*Matrix(U) = Matrix(G)*Matrix(Quo) + Matrix(Rem)`
 and `Rem = normalform(I, G)`. `U` is a diagonal matrix of units differing
 from the identity matrix only for local ring orderings.
 """
