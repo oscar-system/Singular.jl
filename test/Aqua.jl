700dfbbfc732b76a5cecd6a251d6e26bef349ec3
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-11-17 09:51:34 +0100

    Bump Aqua and use more defaults (#731)

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -5,11 +5,6 @@ using Aqua
       Singular;
       ambiguities=false,         # TODO: fix ambiguities
       unbound_args=false,        # TODO: fix unbound args
-      undefined_exports=true,
-      project_extras=true,
-      stale_deps=true,
-      deps_compat=true,
-      project_toml_formatting=true,
-      piracy=false               # TODO: fix piracy
+      piracies=false             # TODO: fix piracy
    )
 end
