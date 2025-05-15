48b753dc9a8899d1e3f754514de123b6d1d6e4fe
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2025-05-15 11:45:33 +0200

    changed doc string

--- src/module/module.jl
+++ src/module/module.jl
@@ -660,10 +660,10 @@ end
 @doc raw"""
     dimension(M::smodule{spoly{T}}) where T <: Nemo.RingElem
 
-Given a module $M$ this function computes the Krull dimension
-of the ring $R^n/M$, where $R$ is the polynomial ring over
-which $M$ is a module and $n$ the rank. The module must be 
-given by a Groebner basis.
+Given a module $M\subseteq R^n$ this function computes the Krull dimension
+of the module $M$, where $R$ is the polynomial ring over
+which $M$ is a module and $n$ the rank if the free module it is embedded in.
+The module must be given by a Groebner basis.
 """
 function dimension(M::smodule{spoly{T}}) where T <: Nemo.RingElem
    M.isGB || error("Not a Groebner basis")
