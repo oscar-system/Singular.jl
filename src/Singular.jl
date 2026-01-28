492e5d3172acd30074783c8e24b5a8b6b1eaaac0
Max Horn <max@quendi.de>
2026-01-28 20:08:03 +0100

    Update docs link in package module docstring

--- src/Singular.jl
+++ src/Singular.jl
@@ -1,7 +1,7 @@
 @doc raw"""
   Julia package for using the Singular library for commutative and non-commutative algebra, algebraic geometry, and singularity theory.
 
-  For documentation see https://oscar-system.github.io/Singular.jl/latest/index.html
+  For documentation see https://oscar-system.github.io/Singular.jl/stable/
 
   For more information about Singular see https://www.singular.uni-kl.de/
 """
