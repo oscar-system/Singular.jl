c42875d79b271c8d2037b97dc802f23c8cc8968a
Janko Boehm <boehm@mathematik.uni-kl.de>
2026-09-17 13:31:53 +0200

    Detect incompatible omalloc page sizes

--- src/LibSingular.jl
+++ src/LibSingular.jl
@@ -2,12 +2,28 @@ module libSingular
 
 using CxxWrap
 
-import ..Singular: libflint, libsingular_julia, AbstractAlgebra
+import ..Singular: libflint, libsingular_julia, AbstractAlgebra, Setup
 
 @wrapmodule(libsingular_julia)
 
+function _check_omalloc_page_size(wrapper_page_size::Integer, singular_page_size::Integer)
+   wrapper_page_size == singular_page_size && return nothing
+   error(
+      "Incompatible omalloc page sizes: Singular.jl requires $(wrapper_page_size) " *
+      "bytes, but the selected Singular_jll was built for $(singular_page_size) bytes. " *
+      "Rebuild libsingular_julia against the selected Singular_jll."
+   )
+end
+
+_check_omalloc_page_size(config_path::AbstractString) =
+   _check_omalloc_page_size(omalloc_page_size(), Setup.omalloc_page_size(config_path))
+
+_check_omalloc_page_size() =
+   _check_omalloc_page_size(omalloc_page_size(), Setup.omalloc_page_size())
+
 function __init__()
    @initcxx
+   _check_omalloc_page_size()
    initialize_jl_c_types(@__MODULE__)
 end
 
